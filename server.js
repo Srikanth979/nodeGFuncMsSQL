1f3124172296ee4ed4f81f55a54c1825e23f02b3
Srikanth979 <srikanth979@gmail.com>
2019-04-02 11:28:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'srikanth_akurathi',
+        password: 'myawspassword456',
+        server: 'techtokawsdb.c6ekpfjoxf9h.us-east-2.rds.amazonaws.com', 
+        database: 'sampleDB' 
+    };
+
+    sql.close();
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from sampleTable', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
